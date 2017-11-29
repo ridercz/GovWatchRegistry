520174c7117f28536f55a5c733b7124a2c282901
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-29 23:38:12 +0100

    Added infrastructure for paging

---
+++ Altairis.GovWatch.Registry.Web/Components/PagerViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Altairis.GovWatch.Registry.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altairis.GovWatch.Registry.Web.Components {
+    public class PagerViewComponent : ViewComponent {
+
+        public IViewComponentResult Invoke(PagingInfo model) {
+            return this.View(model);
+        }
+
+    }
+}
