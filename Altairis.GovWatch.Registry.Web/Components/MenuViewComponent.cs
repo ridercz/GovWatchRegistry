b87a36bcbd4582d9c023f777f86df661ef45f624
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-12-25 16:16:31 +0100

    Added main menu

---
+++ Altairis.GovWatch.Registry.Web/Components/MenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Altairis.GovWatch.Registry.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altairis.GovWatch.Registry.Web.Components {
+    public class MenuViewComponent : ViewComponent {
+
+        public IViewComponentResult Invoke() {
+            if (!this.User.Identity.IsAuthenticated) {
+                return this.View("Anonymous");
+            }
+            else if (this.User.IsInRole(ApplicationRole.AdministratorRoleName)) {
+                return this.View("Administrator");
+            } else if (this.User.IsInRole(ApplicationRole.OperatorRoleName)) {
+                return this.View("Operator");
+            } else {
+                throw new Exception("Unsupported user role for menu display");
+            }
+        }
+
+    }
+}
