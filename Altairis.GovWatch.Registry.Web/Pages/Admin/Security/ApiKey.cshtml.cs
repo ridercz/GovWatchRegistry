9295e4322e14d13afec541058f326a0d61efa45e
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-12-25 16:53:10 +0100

    Added API key page

---
+++ Altairis.GovWatch.Registry.Web/Pages/Admin/Security/ApiKey.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Altairis.GovWatch.Registry.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Altairis.GovWatch.Registry.Web.Pages.Admin.Security {
+    public class ApiKeyModel : PageModel {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ApiKeyModel(UserManager<ApplicationUser> userManager) {
+            _userManager = userManager;
+        }
+
+        public string Key { get; set; }
+
+        public async Task OnGetAsync() {
+            this.Key = await this.GetKey(reset: false);
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            await this.GetKey(reset: true);
+            return this.RedirectToPage();
+        }
+
+        private async Task<string> GetKey(bool reset) {
+            var currentUser = await _userManager.GetUserAsync(this.User);
+            if (reset || string.IsNullOrWhiteSpace(currentUser.ApiKey)) {
+                using (var rng = System.Security.Cryptography.RandomNumberGenerator.Create()) {
+                    var keyBytes = new byte[48];
+                    rng.GetBytes(keyBytes);
+                    currentUser.ApiKey = Convert.ToBase64String(keyBytes).Replace('+', '-').Replace('/', '_');
+                    await _userManager.UpdateAsync(currentUser);
+                }
+            }
+            return currentUser.ApiKey;
+        }
+
+    }
+}
